bae94a631fdf647bfec21375f66533ed6d01ca2f
Adde <addebyn@gmail.com>
2022-11-26 11:40:36 +0100

    created build folder

--- src/markup/pages/index-2.jsx
+++ src/markup/pages/index-2.jsx
@@ -5,10 +5,7 @@ import Header from "../layout/header/header1";
 import Footer from "../layout/footer/footer1";
 
 // Elements
-import ServicesContent2 from '../elements/services-content-2';
-import EventsContent2 from '../elements/events-content2';
-import RecentNewsSlider from '../elements/recent-news-slider';
-import Testimonial3 from '../elements/testimonial3';
+
 import ContactInfoCard from '../elements/contact-info-card';
 import TotalPeople from '../pages/total-people'
 import Start from '../../Start';
@@ -26,12 +23,8 @@ class Index2 extends Component{
 				
 					<div className="content-block" id="content-area">
 					<Start />
-					<ServicesContent2 />
-					<RecentNewsSlider />
 					<TotalPeople />
-					<Testimonial3 />
 					<ContactInfoCard />
-	
 					</div>
 					
 				</div>				
