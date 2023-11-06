4866d7412664b155e5e9c0ccd923bdbec30c0d64
Dalmawi <website@kistaschool.se>
2023-11-06 11:54:05 +0100

    Rinkeby Västerby backe

--- src/markup/pages/laxhjalp.jsx
+++ src/markup/pages/laxhjalp.jsx
@@ -26,6 +26,8 @@ class Laxhjalp extends Component{
 											<h4>Rinkeby folkets hus:</h4>
 												<p>Måndagar kl. 17.00 - 19.00 för elever i åk 4-7.</p>
 												<p>Lördagar och söndagar kl. 15.00-17.00 för elever i åk 8 och 9.</p>
+											<h4>Rinkeby Västerby backe 6:</h4>
+												<p>Onsdagar kl. 17.00 - 19.00</p>
 											<h4>Tensta (Tensta träff):</h4>
 												<p>Tisdagar och torsdagar kl. 17.00 - 19.00</p>
 											<h4>Husby (Folkets Husby):</h4>
