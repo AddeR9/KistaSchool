a58be11f62c5f147ee625f72f97e268bc403bf48
Dalmawi <website@kistaschool.se>
2024-08-06 11:26:54 +0200

    Added change on start

--- src/Start.jsx
+++ src/Start.jsx
@@ -42,6 +42,10 @@ class Start extends Component{
 								<div className="heading-bx style1 mb-3">
 									<h3 className="title-head">Kista International School</h3>
 									<p className="text-white">Kista International School kan stoltsera sig med att tillhöra de skolor som uppnår goda kunskapsresultat varje år i Sverige med hänsyn taget till barnens socioekonomiska förutsättningar. Framgångsfaktorerna är flera; föreningen som engagerad och seriös huvudman, drivkraft och stabilitet i skolledningen, ett stort engagemang bland våra anställda, engagerade och samarbetsvilliga föräldrar, samt viljan att utveckla och förbättra de olika skolverksamheterna.</p>
+									<br/>
+									<p className="text-white">Eleverna i skolans studieråd beskriver skolans studiekultur så här.</p>
+									<p className="text-white">“På Kista International School har vi lugn och ro för att alla ska kunna göra sitt bästa. Eleverna bidrar genom att fokusera på sitt eget lärande. Vi har rätt material med oss och kommer i tid. Med studiero kan lärarna undervisa varierande och får mer tid att lära ut. Alla samarbetar och hjälper varandra. Vi välkomnar nya elever och lärare så att alla känner sig trygga.”</p>
+									<br/>
 								</div>
 								<Link to={{ pathname: "https://apply.meitner.se/organizations/27e927c8-f648-4110-8be1-fea6a3acce4c/schools/38690f73-b7af-4d3e-a457-e0ae7818484c/applications/new" }} target="_blank" className="btn">Ansök</Link>
 							</div>
