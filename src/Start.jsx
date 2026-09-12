82d4915951364c2b90515f65047288e1773553d3
unknown <Dalmawi>
2026-09-13 00:40:44 +0200

    Texten korrigerad

--- src/Start.jsx
+++ src/Start.jsx
@@ -41,9 +41,9 @@ class Start extends Component{
 							<div className="col-lg-5 col-md-12 text-white m-b30">
 								<div className="heading-bx style1 mb-3">
 									<h3 className="title-head">Kista International School</h3>
-									<p className="text-white">Kista International School kan stoltsera sig med att tillhöra de skolor som uppnår goda kunskapsresultat varje år i Sverige med hänsyn taget till barnens socioekonomiska förutsättningar. Framgångsfaktorerna är flera; föreningen som engagerad och seriös huvudman, drivkraft och stabilitet i skolledningen, ett stort engagemang bland våra anställda, engagerade och samarbetsvilliga föräldrar, samt viljan att utveckla och förbättra de olika skolverksamheterna.</p>
+									<p className="text-white">Kista International School kan stoltsera med att tillhöra de tre, fyra bästa skolorna i Sverige år efter år, enligt Skolverkets SALSA-mätning, när det gäller elevernas kunskapsresultat med hänsyn taget till socioekonomiska förutsättningar. Framgångsfaktorerna är flera; föreningen som engagerad och seriös huvudman, drivkraft och stabilitet i skolledningen, ett stort engagemang bland våra anställda, engagerade och samarbetsvilliga föräldrar, samt viljan att ständigt utveckla och förbättra de olika skolverksamheterna.</p>
 									<br/>
-									<p className="text-white">Eleverna i skolans studieråd beskriver skolans studiekultur så här.</p>
+									<p className="text-white">Elever beskriver skolans studiekultur så här.</p>
 									<p className="text-white">“På Kista International School har vi lugn och ro för att alla ska kunna göra sitt bästa. Eleverna bidrar genom att fokusera på sitt eget lärande. Vi har rätt material med oss och kommer i tid. Med studiero kan lärarna undervisa varierande och får mer tid att lära ut. Alla samarbetar och hjälper varandra. Vi välkomnar nya elever och lärare så att alla känner sig trygga.”</p>
 									<br/>
 								</div>
